215475a8a833f74679b611f3107f381e28578c3e
SakT1122 <sak.978tirbhane@gmail.com>
2023-01-21 12:00:48 +0530

    Sub1

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -5,10 +5,23 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
-
-    public Pizza(Boolean isVeg){
+    private int extraCheeseCost;
+    private int extraToppingsCost;
+    private int extraTakewayCost;
+    private boolean extraCheeseAdded;
+    private boolean extraToppingsAdded;
+    private boolean isBillGenerated;
+    private boolean extraTakewayAdded;
+    public Pizza(Boolean isVeg) {
         this.isVeg = isVeg;
-        // your code goes here
+        bill = "";
+        price = (isVeg) ? 300 : 400;
+        extraCheeseCost = 80;
+        extraToppingsCost = (isVeg) ? 70 : 120;
+        extraTakewayCost = 20;
+        extraCheeseAdded = false;
+        extraToppingsAdded = false;
+        isBillGenerated = false;
     }
 
     public int getPrice(){
@@ -17,18 +30,38 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        if(!extraCheeseAdded){
+            price+=extraCheeseCost;
+            extraCheeseAdded=true;
+        }
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(!extraToppingsAdded){
+            price+=extraToppingsCost;
+            extraToppingsAdded=true;
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        if(!extraTakewayAdded){
+            price+=extraTakewayCost;
+            extraTakewayAdded=true;
+        }
     }
 
     public String getBill(){
         // your code goes here
+        if(!isBillGenerated){
+            bill+="Base Price Of The Pizza: "+((isVeg)?300:400)+"\n";
+            bill+=(!extraCheeseAdded)?"":"Extra Cheese Added: "+extraCheeseCost+"\n";
+            bill+=(!extraToppingsAdded)?"":"Extra Toppings Added: "+extraToppingsCost+"\n";
+            bill+=(!extraTakewayAdded)?"":"Paperbag Added: "+extraTakewayCost+"\n";
+            bill+="Total Price: "+String.valueOf(price)+"\n";
+            isBillGenerated=true;
+        }
         return this.bill;
     }
 }
